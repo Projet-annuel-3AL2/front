f6e44fb683a878657ddaf2dbc5a3f15c90d2e5c0
pooetitu <pooetitu@gmail.com>
2021-05-26 23:13:17 +0200

    Add event suggestion component

---
+++ src/app/components/event-suggestion/event-suggestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-suggestion',
+  templateUrl: './event-suggestion.component.html',
+  styleUrls: ['./event-suggestion.component.css']
+})
+export class EventSuggestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
