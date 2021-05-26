66db7cfd41b903582601a2809dbebc22c728f8ce
pooetitu <pooetitu@gmail.com>
2021-05-26 23:14:35 +0200

    Add components declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {PostComponent} from './post/post.component';
+import {PostComponent} from './components/post/post.component';
 import {RouterModule} from "@angular/router";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
@@ -10,16 +10,22 @@ import {TooltipModule} from "ngx-bootstrap/tooltip";
 import {ModalModule} from "ngx-bootstrap/modal";
 import {CommonModule} from "@angular/common";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NavbarComponent} from './navbar/navbar.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {CollapseModule} from "ngx-bootstrap/collapse";
 import {MaterialModule} from "./material.module";
 import {AppRoutingModule} from "./app-routing.module";
+import {TimelineComponent} from "./components/timeline/timeline.component";
+import {EventSuggestionComponent} from "./components/event-suggestion/event-suggestion.component";
+import {EventSuggestionListComponent} from "./components/event-suggestion-list/event-suggestion-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    NavbarComponent
+    NavbarComponent,
+    TimelineComponent,
+    EventSuggestionComponent,
+    EventSuggestionListComponent
   ],
   imports: [
     BrowserModule,
