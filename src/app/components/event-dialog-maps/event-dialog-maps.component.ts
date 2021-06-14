6c9940d927ab77136db70a6afc327664dc143715
Briand <briand.thibaud99@gmail.com>
2021-06-14 12:03:35 +0200

    [add] EventDialogComponent

---
+++ src/app/components/event-dialog-maps/event-dialog-maps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-dialog-maps',
+  templateUrl: './event-dialog-maps.component.html',
+  styleUrls: ['./event-dialog-maps.component.css']
+})
+export class EventDialogMapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
