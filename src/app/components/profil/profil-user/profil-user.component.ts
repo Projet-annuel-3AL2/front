7af712191e254c562535d1a9fec44207f41bf97f
Briand <briand.thibaud99@gmail.com>
2021-06-15 12:04:58 +0200

    [add] UserProfil

--- src/app/components/profil/profil-user/profil-user.component.ts
+++ src/app/components/profil/profil-user/profil-user.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {UserService} from "../../services/user/user.service";
-import {User} from "../../shared/models/user.model";
+import {UserService} from "../../../services/user/user.service";
+import {User} from "../../../shared/models/user.model";
 import {ActivatedRoute} from "@angular/router";
 import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
-import {Post} from "../../shared/models/post.model";
-import {PostService} from "../../services/post/post.service";
+import {Post} from "../../../shared/models/post.model";
+import {PostService} from "../../../services/post/post.service";
+import {Event} from '../../../shared/models/event.model';
 
 @Component({
   selector: 'app-profil-user',
@@ -15,6 +16,8 @@ export class ProfilUserComponent implements OnInit {
   faCheckCircle = faCheckCircle;
   user: User;
   listPost: Post[];
+  listEvent$: Event[];
+  listUser$: User[];
 
   constructor(private userService: UserService,
               private route: ActivatedRoute,
@@ -24,6 +27,8 @@ export class ProfilUserComponent implements OnInit {
     const username = this.route.snapshot.params['username']
     this.getUser(username);
     this.getRelatedPost(username);
+    this.getRelatedEvent(this.user);
+    this.getUserFriends(this.user);
   }
 
   private getUser(username: string) {
@@ -40,5 +45,13 @@ export class ProfilUserComponent implements OnInit {
 
   private getRelatedPost(username: any) {
     this.listPost = this.postService.getUserRelatedPost(username);
+  }
+
+  private getRelatedEvent(user: User) {
+    this.listEvent$ = this.userService.getUserRelatedEvent(user)
+  }
+
+  private getUserFriends(user: User) {
+    this.listUser$ = this.userService.getUserRelatedUser(user)    ;
   }
 }
