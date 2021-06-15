2c22bcc0f4c79eda93921df105c331961761ab45
Briand <briand.thibaud99@gmail.com>
2021-06-15 12:05:10 +0200

    [add] ProfilOrganisation

---
+++ src/app/components/profil/profil-organisation/profil-organisation.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilOrganisationComponent } from './profil-organisation.component';
+
+describe('ProfilOrganisationComponent', () => {
+  let component: ProfilOrganisationComponent;
+  let fixture: ComponentFixture<ProfilOrganisationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilOrganisationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfilOrganisationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/profil/profil-organisation/profil-organisation.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import {Organisation} from "../../../shared/models/organisation.model";
+import {Post} from "../../../shared/models/post.model";
+import {Event} from '../../../shared/models/event.model';
+import {User} from "../../../shared/models/user.model";
+import {OrganisationService} from "../../../services/organisation/organisation.service";
+
+@Component({
+  selector: 'app-profil-organisation',
+  templateUrl: './profil-organisation.component.html',
+  styleUrls: ['./profil-organisation.component.css']
+})
+export class ProfilOrganisationComponent implements OnInit {
+
+  faCheckCircle = faCheckCircle;
+  organisation: Organisation = new Organisation('a','b',undefined);
+  listMember: User[] = [];
+
+  constructor(private organisationService: OrganisationService) { }
+
+  ngOnInit(): void {
+    this.getOrganisation();
+    this.getListMember();
+  }
+
+  isNotAlreadyMember() {
+    return false;
+  }
+
+  askJoin(id: string) {
+
+  }
+
+  private getOrganisation() {
+    this.organisation = this.organisationService.getOrganisation();
+  }
+
+  private getListMember() {
+    this.organisation.members.forEach(membership => {
+      this.listMember.push(membership.user);
+    })
+  }
+}
