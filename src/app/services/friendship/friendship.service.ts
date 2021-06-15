86c82414fb3a30c932764fcd4a18b2cd032bc856
Briand <briand.thibaud99@gmail.com>
2021-06-15 18:01:30 +0200

    [add] friendshipService

---
+++ src/app/services/friendship/friendship.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendshipService {
+
+  constructor(private http: HttpClient) { }
+
+  postFriendship(username: string){
+    this.http.post(`${environment.baseUrl}/friendship/${username}`, null).subscribe({
+      error: error => {
+        if (!environment.production){
+          console.error('There was an error!', error);
+        }
+      }
+    })
+  }
+
+  rejectFriendship(username: string){
+    this.http.delete(`${environment.baseUrl}/friendship/${username}/reject`).subscribe({
+      error: error => {
+        if (!environment.production){
+          console.error('There was an error!', error);
+        }
+      }
+    })
+  }
+
+  removeFriendship(username: string){
+    this.http.delete(`${environment.baseUrl}/friendship/${username}/remove`).subscribe({
+      error: error => {
+        if (!environment.production){
+          console.error('There was an error!', error);
+        }
+      }
+    })
+  }
+
+  putFriendship(username: string){
+    this.http.put(`${environment.baseUrl}/friendship/${username}`, null).subscribe({
+      error: error => {
+        if (!environment.production){
+          console.error('There was an error!', error);
+        }
+      }
+    })
+  }
+}
