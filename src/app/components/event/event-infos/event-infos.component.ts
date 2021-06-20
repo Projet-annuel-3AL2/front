b6c9c1e26a3ceb3e77ed9b1381c376771ece933f
Briand <briand.thibaud99@gmail.com>
2021-06-20 17:05:11 +0200

    Fix event info interface

--- src/app/components/event/event-infos/event-infos.component.ts
+++ src/app/components/event/event-infos/event-infos.component.ts
@@ -19,15 +19,15 @@ export class EventInfosComponent implements OnInit {
   }
 
 
-   getMap() {
-    let map = L.map('eventInfoMap').setView([this.event.latitude, this.event.latitude], 13);
-     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-       maxZoom: 18,
-       id: 'mapbox/streets-v11',
-       tileSize: 512,
-       zoomOffset: -1,
-       accessToken: 'your.mapbox.access.token'
-     }).addTo(map);
-  }
+   // getMap() {
+   //  let map = L.map('eventInfoMap').setView([this.event.latitude, this.event.latitude], 13);
+   //   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+   //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   //     maxZoom: 18,
+   //     id: 'mapbox/streets-v11',
+   //     tileSize: 512,
+   //     zoomOffset: -1,
+   //     accessToken: 'your.mapbox.access.token'
+   //   }).addTo(map);
+  // }
 }
