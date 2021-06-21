6cc42d306c955fd8582671d83f06bf537cce7573
Briand <briand.thibaud99@gmail.com>
2021-06-21 10:03:23 +0200

    Fix buttonAddFriend and buttonFollow

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../shared/models/user.model";
 import {FriendshipService} from "../../services/friendship/friendship.service";
-
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
@@ -10,6 +10,7 @@ import {FriendshipService} from "../../services/friendship/friendship.service";
 export class UserCardComponent implements OnInit {
 
   @Input('user') user: User = new User()
+  faCheckCircle = faCheckCircle;
   constructor(private friendshipService: FriendshipService) { }
 
   ngOnInit(): void {
