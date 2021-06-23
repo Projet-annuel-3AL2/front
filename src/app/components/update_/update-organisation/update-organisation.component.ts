d93f08100f54e6a1b3f1e1824c2bb592ec10e031
Briand <briand.thibaud99@gmail.com>
2021-06-23 15:11:36 +0200

    Init Components

---
+++ src/app/components/update_/update-organisation/update-organisation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-organisation',
+  templateUrl: './update-organisation.component.html',
+  styleUrls: ['./update-organisation.component.css']
+})
+export class UpdateOrganisationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
