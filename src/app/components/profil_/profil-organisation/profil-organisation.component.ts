2185bf968556297c69d85c6ae541bf65eddbbcc9
Briand <briand.thibaud99@gmail.com>
2021-06-24 11:36:07 +0200

    Refacto profil_/ component

--- src/app/components/profil_/profil-organisation/profil-organisation.component.ts
+++ src/app/components/profil_/profil-organisation/profil-organisation.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { faCheckCircle, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import {Organisation} from "../../../shared/models/organisation.model";
-import {Post} from "../../../shared/models/post.model";
-import {Event} from '../../../shared/models/event.model';
 import {User} from "../../../shared/models/user.model";
 import {OrganisationService} from "../../../services/organisation/organisation.service";
 import {ActivatedRoute} from "@angular/router";
 import {environment} from "../../../../environments/environment";
 import {UserService} from "../../../services/user/user.service";
+import {AuthService} from "../../../services/auth/auth.service";
 
 @Component({
   selector: 'app-profil-organisation',
@@ -17,31 +16,30 @@ import {UserService} from "../../../services/user/user.service";
 export class ProfilOrganisationComponent implements OnInit {
 
   faCheckCircle = faCheckCircle;
-  organisation$: Organisation = new Organisation();
-  listMember: User[] = [];
+  organisation$: Organisation;
   organisationName: string;
   faEllipsisH = faEllipsisH;
   user: User;
-  constructor(private organisationService: OrganisationService,
+  listMember$: User[];
+
+  constructor(private _organisationService: OrganisationService,
               private route: ActivatedRoute,
-              private userService: UserService,
-              // private authService: AuthService
+              private _userService: UserService,
+              private _authService: AuthService
   ) { }
 
   ngOnInit(): void {
     this.organisationName = this.route.snapshot.params['organisationName']
-    this.organisation$ = this.organisationService.fakeGetOrganisation();
-    this.listMember = this.userService.fakeGetParticipants("a");
-    this.user = this.userService.fakeGetUser("a");
-    // this.userService.getById(this.authService.getCurrentUserId()).subscribe(user=>{
-    //   this.user=user;
-    // });
+
+    this._userService.getById(this._authService.getCurrentUserId()).subscribe(user=>{
+      this.user=user;
+    });
     // this.getOrganisation();
     // this.getListMember();
   }
 
   private getOrganisation() {
-    this.organisationService.getOrganisationByName(this.organisationName).subscribe({
+    this._organisationService.getOrganisationByName(this.organisationName).subscribe({
       next: organisation => {
         this.organisation$ = organisation
       },
@@ -54,8 +52,17 @@ export class ProfilOrganisationComponent implements OnInit {
   }
 
   private getListMember() {
-    this.organisation$.members.forEach(membership => {
-      this.listMember.push(membership.user);
+    this._organisationService.getOrganisationMembership(this.organisationName).subscribe({
+      next: listOrganisationMembership => {
+        listOrganisationMembership.forEach(organisationMembership => {
+          this.listMember$.push(organisationMembership.user);
+        })
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
     })
   }
 
@@ -72,6 +79,7 @@ export class ProfilOrganisationComponent implements OnInit {
 
   }
 
+  // TODO : isOwner pour modification Organisation
   isOwner() {
     return true;
     // return this.organisation$.owner.id == this.user.id;
--- src/app/components/profil_/profil-user/profil-user.component.ts
+++ src/app/components/profil_/profil-user/profil-user.component.ts
@@ -9,6 +9,7 @@ import {Event} from '../../../shared/models/event.model';
 import {environment} from "../../../../environments/environment";
 import {FriendshipService} from "../../../services/friendship/friendship.service";
 import {EventService} from "../../../services/event/event.service";
+import {AuthService} from "../../../services/auth/auth.service";
 
 @Component({
   selector: 'app-profil-user',
@@ -18,40 +19,49 @@ import {EventService} from "../../../services/event/event.service";
 export class ProfilUserComponent implements OnInit {
   faCheckCircle = faCheckCircle;
   faEllipsisH = faEllipsisH;
-  user: User;
+  user$: User;
+  authenticateUser$: User;
   listPost$: Post[];
   listEvent$: Event[];
   listUser$: User[];
 
-  constructor(private userService: UserService,
+  constructor(private _userService: UserService,
               private route: ActivatedRoute,
-              private postService: PostService,
-              private friendshipService: FriendshipService,
-              private eventService: EventService,
-              // private authService: AuthService
-              ) { }
+              private _postService: PostService,
+              private _friendshipService: FriendshipService,
+              private _eventService: EventService,
+              private _authService: AuthService
+  ) { }
 
   ngOnInit(): void {
-    this.user = this.userService.fakeGetUser('a');
-    this.listPost$ = this.postService.fakeGetRelatedPost('a');
-    this.listEvent$ = this.eventService.fakeGetEvents();
-    this.listUser$ = this.userService.fakeGetParticipants('a');
-    // const username = this.route.snapshot.params['username']
-    // this.userService.getById(this.authService.getCurrentUserId()).subscribe(user=>{
-    //   this.user=user;
-    // });
+    const username = this.route.snapshot.params['username'];
+
+    this._userService.getById(this._authService.getCurrentUserId()).subscribe(user=>{
+      this.authenticateUser$=user;
+
+    });
+    // TODO: fonction pas activé
     // this.getUser(username);
     // this.getRelatedPost(username);
-    // this.getRelatedEvent(this.user);
-    // this.getUserFriends(this.user);
+    // this.getRelatedEvent(this.user.id);
+    // this.getUserFriends(this.user.id);
   }
 
   private getUser(username: string) {
-    this.user = this.userService.fakeGetUser('1');
+    this._userService.getByName(username).subscribe({
+      next: user => {
+        this.user$ = user
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    })
   }
 
   private getRelatedPost(username: any) {
-    this.postService.getPostByUsername(username).subscribe({
+    this._postService.getPostByUsername(username).subscribe({
       next: posts => {
         this.listPost$ = posts
       },
@@ -63,24 +73,41 @@ export class ProfilUserComponent implements OnInit {
     })
   }
 
-  private getRelatedEvent(user: User) {
-    this.listEvent$ = this.userService.getUserRelatedEvent(user)
+  private getRelatedEvent(userId: string) {
+    this._eventService.getEventRelatedToUser(userId).subscribe({
+      next: events => {
+        this.listEvent$ = events
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    });
   }
 
-  private getUserFriends(user: User) {
-    this.listUser$ = this.userService.getUserRelatedUser(user);
+  private getUserFriends(userId: string) {
+    this._userService.getUserFriends(userId).subscribe({
+      next: users => {
+        this.listUser$ = users
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    });
   }
 
   // TODO : Logique de Un user peut ajouter ou non un amis (voir list d'amis)
-
   canAdd(id: string) {
     return false;
   }
   dellFriend(username: string) {
-    // this.friendshipService.removeFriendship(username);
+    // this._friendshipService.removeFriendship(username);
   }
 
   askFriend(username: string) {
-    // this.friendshipService.postFriendship(username);
+    // this._friendshipService.postFriendship(username);
   }
 }
