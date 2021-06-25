5dc6ea3e6b74882d3a8e7b9cdffee63f46a16133
Briand <briand.thibaud99@gmail.com>
2021-06-25 16:24:59 +0200

    Implement card-user-manage-orga

---
+++ src/app/components/card_/card-user-manage-organisation/card-user-manage-organisation.component.ts
@@ -0,0 +1,107 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../../shared/models/user.model";
+import {FriendshipService} from "../../../services/friendship/friendship.service";
+import {EventService} from "../../../services/event/event.service";
+import {AuthService} from "../../../services/auth/auth.service";
+import {UserService} from "../../../services/user/user.service";
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import {OrganisationService} from "../../../services/organisation/organisation.service";
+import {Organisation} from "../../../shared/models/organisation.model";
+import {environment} from "../../../../environments/environment";
+
+@Component({
+  selector: 'app-card-user-manage-organisation',
+  templateUrl: './card-user-manage-organisation.component.html',
+  styleUrls: ['./card-user-manage-organisation.component.css']
+})
+export class CardUserManageOrganisationComponent implements OnInit {
+
+  @Input('user') user: User = new User();
+  @Input('organisation') organisation: Organisation;
+  faCheckCircle = faCheckCircle;
+  userSession: User;
+
+  constructor(private _friendshipService: FriendshipService,
+              private _authService: AuthService,
+              private _userService: UserService,
+              private _organisationService: OrganisationService) { }
+
+  ngOnInit(): void {
+    this._userService.getById(this._authService.getCurrentUserId()).subscribe(user=>{
+      this.userSession=user;
+    });
+  }
+
+  // TODO : Logique de Un user peut ajouter ou non un amis (voir list d'amis)
+  canAdd(userId: string) {
+    return true;
+  }
+
+  askFriend(username: string) {
+    // this._friendshipService.postFriendship(username);
+  }
+
+  dellFriend(username: string) {
+    // this._friendshipService.removeFriendship(username)
+  }
+
+  deleteMembership(userId: string) {
+    this._organisationService.deleteOrganisationMembership(userId, this.organisation.id);
+  }
+
+  // TODO: IsOwner() dela page-event
+  isOwner() {
+    return true;
+    // return this.event.creator.id == this.user.id;
+  }
+
+  // TODO: canRemove from Organisation la
+  canRemoveUser(id: string): Boolean {
+    let canRemove = false;
+
+    // this._organisationService.getOrganisationMembership(this.organisation.id).subscribe({
+    //   next: listMemberShip => {
+    //     listMemberShip.forEach(member => {
+    //       if (member.user.id == this.user.id && member.isAdmin){
+    //         canRemove = true;
+    //       }
+    //     })
+    //   },
+    //   error: error => {
+    //     if (!environment.production) {
+    //       console.error('Error: ', error);
+    //     }
+    //   }
+    // })
+    return canRemove;
+  }
+
+  userIsAdmin(userId: string) {
+    let isAdmin = false;
+
+    this._organisationService.getOrganisationMembership(this.organisation.id).subscribe({
+      next: listMemberShip => {
+        listMemberShip.forEach(member => {
+          if (member.user.id == userId && member.isAdmin){
+            isAdmin = true;
+          }
+        })
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    })
+    return isAdmin;
+  }
+
+  giveAdmin(userId: string) {
+    this._organisationService.giveAdminToMember(userId, this.organisation.id);
+  }
+
+  removeAdmin(userId: string) {
+    this._organisationService.removeAdminToAdminMember(userId, this.organisation.id);
+  }
+
+}
---
+++ src/app/components/card_/card-user-mange-event/card-user-mange-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-user-mange-event',
+  templateUrl: './card-user-mange-event.component.html',
+  styleUrls: ['./card-user-mange-event.component.css']
+})
+export class CardUserMangeEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
