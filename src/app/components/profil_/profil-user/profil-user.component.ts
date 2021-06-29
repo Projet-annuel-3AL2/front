c4b8b959a6e90aa3c589a08df6c7c3215403bf9e
Briand <briand.thibaud99@gmail.com>
2021-06-30 00:46:27 +0200

    Refacto profil-user component

--- src/app/components/profil_/profil-user/profil-user.component.ts
+++ src/app/components/profil_/profil-user/profil-user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../services/user/user.service";
 import {User} from "../../../shared/models/user.model";
 import {ActivatedRoute} from "@angular/router";
@@ -10,6 +10,9 @@ import {environment} from "../../../../environments/environment";
 import {FriendshipService} from "../../../services/friendship/friendship.service";
 import {EventService} from "../../../services/event/event.service";
 import {AuthService} from "../../../services/auth/auth.service";
+import {FriendRequestStatus} from "../../../shared/FriendshipRequestStatus.enum";
+import {DialogResFriendshipRequestComponent} from "../../dialog_/dialog-res-friendship-request/dialog-res-friendship-request.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-profil-user',
@@ -19,19 +22,22 @@ import {AuthService} from "../../../services/auth/auth.service";
 export class ProfilUserComponent implements OnInit {
   faCheckCircle = faCheckCircle;
   faEllipsisH = faEllipsisH;
+
   user$: User;
   userSession: User;
-  listPost$: Post[];
-  listEvent$: Event[];
-  listUser$: User[];
-  isCanAdd: boolean = true;
+  listPost$: Post[] = [];
+  listEvent$: Event[] = [];
+  listUser$: User[] = [];
+  friendshipRequest: FriendRequestStatus = FriendRequestStatus.NONE;
+  allFriendRequestStatus = FriendRequestStatus;
 
   constructor(private _userService: UserService,
               private route: ActivatedRoute,
               private _postService: PostService,
               private _friendshipService: FriendshipService,
               private _eventService: EventService,
-              private _authService: AuthService
+              private _authService: AuthService,
+              public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -39,19 +45,21 @@ export class ProfilUserComponent implements OnInit {
 
     this._userService.getById(this._authService.getCurrentUserId()).subscribe(user=>{
       this.userSession=user;
-
     });
-    // TODO: fonction pas activé
     this.getFullUser(username);
-    // this.getUserFriends(this.user.id);
-    this.canAdd();
+
   }
 
   private getFullUser(username: string) {
-    this._userService.getFullUser(username).subscribe({
+    this._userService.getById(username).subscribe({
       next: user => {
         this.user$ = user
         this.getTimeline(user.id);
+        this.getFriendsList();
+        this.getEventParticipations();
+        if (user.id != this.userSession.id){
+          this.canAdd();
+        }
       },
       error: error => {
         if (!environment.production) {
@@ -74,15 +82,74 @@ export class ProfilUserComponent implements OnInit {
     })
   }
 
-  // TODO : Logique de Un user peut ajouter ou non un amis (voir list d'amis)
-  canAdd() {
-    this.isCanAdd = true;
+  private getFriendsList() {
+    this._userService.getFriends(this.user$.username).subscribe({
+      next: users =>{
+        this.listUser$ = users;
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    })
   }
-  dellFriend(username: string) {
-    // this._friendshipService.removeFriendship(username);
+
+  canAdd() {
+    this._friendshipService.isFriendshipRequested(this.user$.username).subscribe({
+      next: requestStatus => {
+        this.friendshipRequest = requestStatus;
+      }
+    })
+  }
+
+  dellFriend() {
+    this._friendshipService.postFriendship(this.user$.username).subscribe({
+      next: () => {
+        this.friendshipRequest = FriendRequestStatus.PENDING;
+      },
+      error: err => {
+        if (!environment.production){
+          console.log(err)
+        }
+      }
+    });
   }
 
   askFriend(username: string) {
-    // this._friendshipService.postFriendship(username);
+    this._friendshipService.removeFriendship(this.user$.username).subscribe({
+      next: () => {
+        this.friendshipRequest = FriendRequestStatus.NONE;
+      },
+      error: err => {
+        if (!environment.production){
+          console.log(err)
+        }
+      }
+    })
+  }
+
+  private getEventParticipations() {
+    this._userService.getParticipations(this.user$.username).subscribe({
+      next: events =>{
+        this.listEvent$ = events;
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    })
+  }
+
+  showDialogueRespondFriendRequest() {
+    const dialogRef = this.dialog.open(DialogResFriendshipRequestComponent, {
+      width: '500px',
+      data: {userId: this.user$.username}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.friendshipRequest = result;
+    })
   }
 }
