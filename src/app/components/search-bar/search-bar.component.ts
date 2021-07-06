18e0e3cece57d65deb27589433acd54b3492d5c9
pooetitu <pooetitu@gmail.com>
2021-07-06 16:33:48 +0200

    Implement search bar treatment

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {SearchResult} from "../../shared/models/search.model";
+import {FormControl} from "@angular/forms";
+import {SearchService} from "../../services/search/search.service";
 
 @Component({
   selector: 'app-search-bar',
@@ -8,9 +11,19 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 })
 export class SearchBarComponent implements OnInit {
   faSearch = faSearch;
-  constructor() { }
+  searchControl = new FormControl();
+  searchResult: SearchResult;
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
+  }
+
+  searchData($event: any): void{
+    this.searchService.searchData($event.target.value)
+      .subscribe(searchResult=> {
+        this.searchResult = searchResult;
+        console.log(searchResult)
+      });
   }
 
 }
