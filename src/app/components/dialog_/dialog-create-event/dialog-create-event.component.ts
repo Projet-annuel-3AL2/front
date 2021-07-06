a5c9333c99c8efa31b657e54fe8e5d31331e98ed
Briand <briand.thibaud99@gmail.com>
2021-07-06 16:09:21 +0200

    Implement dialog-create-event

---
+++ src/app/components/dialog_/dialog-create-event/dialog-create-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-create-event',
+  templateUrl: './dialog-create-event.component.html',
+  styleUrls: ['./dialog-create-event.component.css']
+})
+export class DialogCreateEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
