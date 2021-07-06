e2f8a99fa2a6b9947ad0476462fb231790863f6c
pooetitu <pooetitu@gmail.com>
2021-07-06 22:24:54 +0200

    Add display comments list and add comment field

--- src/app/components/page_/post-page/post-page.component.ts
+++ src/app/components/page_/post-page/post-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Post} from "../../../shared/models/post.model";
 import {PostService} from "../../../services/post/post.service";
+import {Comment} from "../../../shared/models/comment.model";
+import {UserService} from "../../../services/user/user.service";
+import {AuthService} from "../../../services/auth/auth.service";
 
 @Component({
   selector: 'app-post-page',
@@ -10,14 +13,24 @@ import {PostService} from "../../../services/post/post.service";
 })
 export class PostPageComponent implements OnInit {
   post: Post;
+  comments: Comment[];
   constructor(private _activatedRoute:ActivatedRoute,
-              private _postService: PostService) { }
+              private _postService: PostService,
+              public _userService: UserService,
+              public _authService: AuthService) { }
 
   ngOnInit(): void {
-    this._postService.getPostById(this._activatedRoute.snapshot.paramMap.get("postId"))
-      .subscribe(post=> {
-        this.post = post;
-      });
+    this.update();
   }
 
+ update(): void {
+   this._postService.getPostById(this._activatedRoute.snapshot.paramMap.get("postId"))
+     .subscribe(post=> {
+       this.post = post;
+     });
+   this._postService.getComments(this._activatedRoute.snapshot.paramMap.get("postId"))
+     .subscribe(comments=> {
+       this.comments = comments;
+     });
+ }
 }
