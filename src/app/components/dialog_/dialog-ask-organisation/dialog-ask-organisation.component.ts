eb5b298b21672cabb687986ab43c5fd7d4a84e15
Briand <briandthibaud99@gmail.com>
2021-07-09 00:38:43 +0200

    Implement DialogAskOrganisation

--- src/app/components/dialog_/dialog-ask-organisation/dialog-ask-organisation.component.ts
+++ src/app/components/dialog_/dialog-ask-organisation/dialog-ask-organisation.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {OrganisationRequest} from "../../../shared/models/organisation_request.model";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {OrganisationService} from "../../../services/organisation/organisation.service";
+import {User} from "../../../shared/models/user.model";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-dialog-ask-organisation',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-ask-organisation.component.css']
 })
 export class DialogAskOrganisationComponent implements OnInit {
+  organisationRequest: OrganisationRequest;
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<DialogAskOrganisationComponent>,
+              private _organisationService: OrganisationService,
+              @Inject(MAT_DIALOG_DATA) public data: {user: User}) { }
 
   ngOnInit(): void {
+    this.organisationRequest = new OrganisationRequest();
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onClickSubmit() {
+    this._organisationService.postOrganisationRequest(this.organisationRequest).subscribe({
+      next: () => {
+        this.dialogRef.close()
+      },
+      error: error => {
+        if (!environment.production) {
+          console.error('Error: ', error);
+        }
+      }
+    });
+  }
 }
