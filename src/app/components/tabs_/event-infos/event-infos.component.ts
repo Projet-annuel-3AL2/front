4e688ca2d5e4cd1f87d1588b09d19aa7e76870aa
pooetitu <pooetitu@gmail.com>
2021-07-14 18:26:48 +0200

    Add display of the town and the postcode in event infos

--- src/app/components/tabs_/event-infos/event-infos.component.ts
+++ src/app/components/tabs_/event-infos/event-infos.component.ts
@@ -3,6 +3,7 @@ import {Event} from "../../../shared/models/event.model";
 import {OrganisationService} from "../../../services/organisation/organisation.service";
 import {UserService} from "../../../services/user/user.service";
 import {AuthService} from "../../../services/auth/auth.service";
+import {MapService} from "../../../services/map/map.service";
 
 @Component({
   selector: 'app-event-infos',
@@ -14,14 +15,16 @@ export class EventInfosComponent implements OnInit {
   @Input()
   event: Event = new Event();
   isFollowing: boolean = false;
+  address:any;
 
   constructor(private _organisationService: OrganisationService,
               public _authService: AuthService,
+              public _mapService: MapService,
               private _userService: UserService) {
-
   }
 
   ngOnInit(): void {
+    this._mapService.getAddressFromLatLng(this.event.latitude, this.event.longitude).subscribe(address=>this.address=address);
   }
 
 
