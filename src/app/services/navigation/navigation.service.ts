586e1b826bb2bfcead641c5f1759b8527cae96e7
Briand <briandthibaud99@gmail.com>
2021-07-20 11:05:44 +0200

    Implement return-button

--- src/app/services/navigation/navigation.service.ts
+++ src/app/services/navigation/navigation.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import { Location } from '@angular/common'
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
-  constructor() { }
+  private history: string[] = []
+
+  constructor(private router: Router, private location: Location) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.history.push(event.urlAfterRedirects)
+      }
+    })
+  }
+
+  back(): void {
+    this.history.pop()
+    if (this.history.length > 0) {
+      this.location.back();
+    } else {
+      this.router.navigateByUrl('/')
+    }
+  }
 }
