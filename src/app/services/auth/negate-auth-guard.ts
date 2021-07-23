006d3a124a2a56c47686c5c6f1ba2404545e7e49
pooetitu <pooetitu@gmail.com>
2021-07-23 21:09:29 +0200

    Fix guards implementations

--- src/app/services/auth/negate-auth-guard.ts
+++ src/app/services/auth/negate-auth-guard.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
+import {CanActivate, Router, UrlTree} from '@angular/router';
 import {AuthService} from "./auth.service";
 
 @Injectable({
@@ -9,11 +9,7 @@ export class NegateAuthGuardService implements CanActivate {
   constructor(private auth: AuthService, public router: Router) {
   }
 
-  canActivate(): boolean {
-    if (this.auth.isAuthenticated()) {
-      this.router.navigate(['']);
-      return false;
-    }
-    return true;
+  canActivate(): boolean | UrlTree {
+    return this.auth.isAuthenticated() ? this.router.parseUrl("/") : true;
   }
 }
